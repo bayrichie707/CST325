67991d92a506fb5eb274c5f4a7cf3ccd58d4abee
bayrichie707 <richiealire@yahoo.com>
2022-11-28 22:07:17 -0800

    Working on sampling shadows

--- Week 12 - Shadow Mapping/shaders/phong.fs.glsl
+++ Week 12 - Shadow Mapping/shaders/phong.fs.glsl
@@ -32,10 +32,10 @@ void main(void) {
 
   // todo #5
   // transform the world position into the lights clip space (clip space and NDC will be the same for orthographic projection)
-  // vec4 lightSpaceNDC = ...;
+  vec4 lightSpaceNDC = uLightVPMatrix * vec4(vWorldPosition, 1.0);
 
   // scale and bias the light-space NDC xy coordinates from [-1, 1] to [0, 1]
-  // vec2 lightSpaceUV = ...;
+  vec2 lightSpaceUV = lightSpaceNDC.xy * 0.5 + 0.5;
 
   // todo #6
   // Sample from the shadow map texture using the previously calculated lightSpaceUV
@@ -48,7 +48,8 @@ void main(void) {
   float bias = 0.004;
 
   // todo #8
-  gl_FragColor = vec4(finalColor, 1.0); // remove this when you are ready to add shadows
+  // gl_FragColor = vec4(finalColor, 1.0); // remove this when you are ready to add shadows
+  gl_FragColor = vec4(lightSpaceUV.x, lightSpaceUV.y, 0.0, 1.0);
   //if (/* in shadow*/) {
   //   gl_FragColor = vec4(ambient, 1.0);
   //} else {
